ff43e6aab8d9cbfb077dd758d5672fb62cbc944b
ashwin1111 <winash1998@gmail.com>
2020-07-13 17:12:26 +0530

    version update

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='sageintacctsdk',
-    version='0.1.0',
+    version='0.2.0',
     author='Ashwin T',
     author_email='ashwin.t@fyle.in',
     description='Python SDK for accessing Sage Intacct APIs',
