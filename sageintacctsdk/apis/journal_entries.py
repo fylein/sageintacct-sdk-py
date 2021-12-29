403bba55b004bf6fa698908612cfc62701aa7e88
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2021-12-29 20:23:21 +0530

    Support for Updates (#31)

--- sageintacctsdk/apis/journal_entries.py
+++ sageintacctsdk/apis/journal_entries.py
@@ -10,24 +10,3 @@ class JournalEntries(ApiBase):
     """Class for Journal Entries APIs."""
     def __init__(self):
         ApiBase.__init__(self, dimension='GLBATCH')
-
-    def update_attachment(self, recordno: str, supdocid: str):
-        """Update journal entry with supdocid
-
-        Parameters:
-            recordno (str): A parameter to update journal entry by the recordno. (required).
-            supdoc (str): A parameter to update attachment ID for the journal entry. (required).
-
-        Returns:
-            Dict in Journal update schema.
-        """
-        data = {
-            'update': {
-                'GLBATCH': {
-                    'RECORDNO': recordno,
-                    'SUPDOCID': supdocid
-                }
-            }
-        }
-
-        return self.format_and_send_request(data)
