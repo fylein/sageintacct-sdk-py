c33d9b84f7e6603a68ed9dc93dc908bd65edb2ec
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2022-05-12 13:34:06 +0530

    Bump Version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='sageintacctsdk',
-    version='1.9.6',
+    version='1.10.0',
     author='Ashwin T',
     author_email='ashwin.t@fyle.in',
     description='Python SDK for accessing Sage Intacct APIs',
