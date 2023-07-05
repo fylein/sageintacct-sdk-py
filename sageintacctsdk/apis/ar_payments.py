156b98e75615227ac58de3bf046accbfb3ba23fa
Damian <osubuckeye2007@gmail.com>
2023-07-05 10:44:56 -0400

    removed get_all method from ARPayments class per code review, can use the get_all metohd in the base ApiBase class

--- sageintacctsdk/apis/ar_payments.py
+++ sageintacctsdk/apis/ar_payments.py
@@ -10,20 +10,3 @@ class ARPayments(ApiBase):
     """Class for AR Payments APIs."""
     def __init__(self):
         ApiBase.__init__(self, dimension='ARPYMT')
-
-    def get_all(self):
-        """Get all AR Payments from Sage Intacct
-
-        Returns:
-            List of Dict in AR Payments schema.
-        """
-        data = {
-            'readByQuery': {
-                'object': 'ARPYMT',
-                'fields': '*',
-                'query': None,
-                'pagesize': '1000'
-            }
-        }
-
-        return self.format_and_send_request(data)['data']['arpymt']
