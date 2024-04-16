d1d81f253c1e30530c322b41ab59b2a80e36ba38
ruuushhh <rushikeshtayade2001@gmail.com>
2024-04-16 13:01:21 +0530

    Fix: Get Vendor Call

--- sageintacctsdk/apis/vendors.py
+++ sageintacctsdk/apis/vendors.py
@@ -4,9 +4,40 @@ Sage Intacct vendors
 from typing import Dict
 
 from .api_base import ApiBase
+from .constants import dimensions_fields_mapping
 
 
 class Vendors(ApiBase):
     """Class for Vendors APIs."""
     def __init__(self):
         ApiBase.__init__(self, dimension='VENDOR')
+
+    def get(self, field: str, value: str, fields: list = None):
+        """Get data from Sage Intacct based on filter.
+
+        Parameters:
+            field (str): A parameter to filter by the field. (required).
+            value (str): A parameter to filter by the field - value. (required).
+
+        Returns:
+            Dict.
+        """
+        data = {
+            'query': {
+                'object': 'VENDOR',
+                'select': 
+                    {
+                        'field': fields if fields else dimensions_fields_mapping['VENDOR'],
+                    },
+                'pagesize': '1',
+                'filter': {
+                    'equalto': [
+                    {
+                        'field': field,
+                        'value': value
+                    }]
+                }
+            }
+        }
+
+        return self.format_and_send_request(data)['data']
