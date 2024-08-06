ccf6dde97048176f575145e0866f6d5baa3bb141
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2024-08-06 16:59:16 +0530

    Fix allocation entry fetch (#84)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='sageintacctsdk',
-    version='1.22.2',
+    version='1.22.3',
     author='Ashwin T',
     author_email='ashwin.t@fyle.in',
     description='Python SDK for accessing Sage Intacct APIs',
