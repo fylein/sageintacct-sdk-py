c1f7f2b871ce176c7112ace8d037ed7b0b4f9d66
Ashutosh619-sudo <ashutoshsinghrkt@gmail.com>
2024-09-27 13:34:46 +0530

    feat: Get active counts of attribute

--- sageintacctsdk/apis/api_base.py
+++ sageintacctsdk/apis/api_base.py
@@ -201,6 +201,7 @@ class ApiBase:
 
             if parsed_response['response']['control']['status'] == 'failure':
                 exception_msg = self.__decode_support_id(parsed_response['response']['errormessage'])
+                print(parsed_response)
                 raise WrongParamsError('Some of the parameters are wrong', exception_msg)
 
             if api_response['authentication']['status'] == 'failure':
@@ -357,6 +358,9 @@ class ApiBase:
                 'object': self.__dimension,
                 'select': {
                     'field': 'RECORDNO'
+                },
+                'filter': {
+                    'equalto': {'field': 'STATUS', 'value': 'active'}
                 },
                 'pagesize': '1'
             }
